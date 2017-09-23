0370806e44f1de978bcd5cbdcd5ed6610eda2fdf
Avery Daniels <averyDC@hotmail.com>
2017-09-23 21:36:37 +1000

    up

---
+++ Captcha Generator/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace Captcha_Generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnChooseFolder = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtAmount = new System.Windows.Forms.TextBox();
+            this.lblChooseFolder = new System.Windows.Forms.Label();
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.txtAttempt = new System.Windows.Forms.TextBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnChooseFolder
+            // 
+            this.btnChooseFolder.Location = new System.Drawing.Point(12, 19);
+            this.btnChooseFolder.Name = "btnChooseFolder";
+            this.btnChooseFolder.Size = new System.Drawing.Size(97, 23);
+            this.btnChooseFolder.TabIndex = 0;
+            this.btnChooseFolder.Text = "Choose Folder...";
+            this.btnChooseFolder.UseVisualStyleBackColor = true;
+            this.btnChooseFolder.Click += new System.EventHandler(this.btnChooseFolder_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(13, 49);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(96, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtAmount
+            // 
+            this.txtAmount.Location = new System.Drawing.Point(186, 52);
+            this.txtAmount.Name = "txtAmount";
+            this.txtAmount.Size = new System.Drawing.Size(100, 20);
+            this.txtAmount.TabIndex = 2;
+            // 
+            // lblChooseFolder
+            // 
+            this.lblChooseFolder.AutoSize = true;
+            this.lblChooseFolder.Location = new System.Drawing.Point(133, 24);
+            this.lblChooseFolder.Name = "lblChooseFolder";
+            this.lblChooseFolder.Size = new System.Drawing.Size(16, 13);
+            this.lblChooseFolder.TabIndex = 3;
+            this.lblChooseFolder.Text = "...";
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.Location = new System.Drawing.Point(133, 55);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(47, 13);
+            this.lblNumber.TabIndex = 4;
+            this.lblNumber.Text = "Number:";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 78);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(316, 222);
+            this.panel1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(350, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Check Captcha";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(353, 250);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 8;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // txtAttempt
+            // 
+            this.txtAttempt.Location = new System.Drawing.Point(435, 252);
+            this.txtAttempt.Name = "txtAttempt";
+            this.txtAttempt.Size = new System.Drawing.Size(164, 20);
+            this.txtAttempt.TabIndex = 9;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(353, 277);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(246, 23);
+            this.btnOpen.TabIndex = 10;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(353, 49);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(246, 195);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(620, 313);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.txtAttempt);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblNumber);
+            this.Controls.Add(this.lblChooseFolder);
+            this.Controls.Add(this.txtAmount);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnChooseFolder);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Captcha Generator";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnChooseFolder;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtAmount;
+        private System.Windows.Forms.Label lblChooseFolder;
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.TextBox txtAttempt;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Captcha Generator/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace Captcha_Generator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<string> Strings = new List<string>();
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            Image[] images = GenerateCaptchas(Convert.ToInt32(txtAmount.Text));
+            int g = 0;
+            foreach (Image image in images)
+            {
+                image.Save(lblChooseFolder.Text + "\\" + Strings[g] + ".png");
+                g++;
+            }
+        }
+
+        Image[] GenerateCaptchas(int amount)
+        {
+            Image[] images = new Image[amount];
+            Random ran = new Random();
+            for (int z = 0; z < amount; z++)
+            {
+                Bitmap bitmap = new Bitmap(panel1.Width, panel1.Height);
+                Graphics g = Graphics.FromImage(bitmap);
+                g.Clear(panel1.BackColor);
+                SolidBrush b = new SolidBrush(Color.FromArgb(0xFF, ran.Next(0, 255), ran.Next(0, 255)));
+                Pen p = new Pen(Color.FromArgb(0xFF, ran.Next(0, 255), ran.Next(0, 255)));
+                char[] chars = "qwertyuiopasdfghjklzxcvbnm1234567890".ToCharArray();
+                string randomString = "";
+                for (int i = 0; i < 6; i++)
+                {
+                    randomString += chars[ran.Next(0, 35)];
+                }
+                byte[] buffer = new byte[randomString.Length];
+                int y = 0;
+                foreach (char c in randomString.ToCharArray())
+                {
+                    buffer[y] = (byte)c;
+                    y++;
+                }
+                MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+                string md5string = BitConverter.ToString(md5.ComputeHash(buffer)).Replace("-", "");
+                Strings.Add(md5string);
+                FontFamily ff = new FontFamily("Arial");
+                Font f = new System.Drawing.Font(ff, 22);
+                g.DrawString(randomString, f, b, 30, 30);
+                for (int i = 0; i < 6; i++)
+                {
+                    int j = ran.Next(0, 2);
+                    if (j == 0)
+                    {
+                        g.DrawRectangle(p, ran.Next(0, 158), ran.Next(0, 60), ran.Next(0, 111), ran.Next(0, 60));
+                    }
+                    else if (j == 1)
+                    {
+                        g.DrawEllipse(p, ran.Next(0, 158), ran.Next(0, 60), ran.Next(0, 111), ran.Next(0, 60));
+                    }
+                    p.Color = Color.FromArgb(0xFF, ran.Next(0, 255), ran.Next(0, 255));
+                }
+                panel1.BackgroundImage = bitmap;
+                images[z] = bitmap;
+            }
+            return images;
+        }
+
+        private void btnChooseFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                lblChooseFolder.Text = fbd.SelectedPath;
+            }
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            int y = 0;
+            byte[] buffer = new byte[txtAttempt.Text.Length];
+            foreach (char c in txtAttempt.Text.ToCharArray())
+            {
+                buffer[y] = (byte)c;
+                y++;
+            }
+            string compare = BitConverter.ToString(md5.ComputeHash(buffer)).Replace("-", "");
+            if (compare != md5HashedName)
+            {
+                MessageBox.Show("Sorry, that is not correct.");
+            }
+            else
+            {
+                MessageBox.Show("Correct.");
+            }
+        }
+        string md5HashedName = "";
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.ImageLocation = ofd.FileName;
+                md5HashedName = Path.GetFileNameWithoutExtension(ofd.FileName);
+            }
+        }
+    }
+}
